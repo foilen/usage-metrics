e30825ddd4083942879b019492aa945f2b73714a
Simon Levesque <simon@foilen.com>
2019-09-23 07:41:20 -0400

    CLOUD-77 When getting an exception, return a json error

---
+++ usage-metrics-central/src/main/java/com/foilen/usagemetrics/central/controller/CentralHandlerExceptionResolver.java
@@ -0,0 +1,32 @@
+/*
+    Usage Metrics
+    https://github.com/foilen/usage-metrics
+    Copyright (c) 2019 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.usagemetrics.central.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.foilen.smalltools.restapi.model.ApiError;
+import com.foilen.smalltools.tools.AbstractBasics;
+
+@Component
+public class CentralHandlerExceptionResolver extends AbstractBasics implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("error", new ApiError(ex.getMessage()));
+        return modelAndView;
+    }
+
+}
