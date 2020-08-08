56e3c1d72425791bf50c8db5ab9f8d1cbabf3937
Simon Levesque <simon@foilen.com>
2020-08-07 21:34:51 -0400

    DockerGrabber - The owner is the container name

--- usage-metrics-agent/src/main/java/com/foilen/usagemetrics/agent/grabber/DockerGrabber.java
+++ usage-metrics-agent/src/main/java/com/foilen/usagemetrics/agent/grabber/DockerGrabber.java
@@ -11,12 +11,9 @@ package com.foilen.usagemetrics.agent.grabber;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.foilen.smalltools.tools.AbstractBasics;
-import com.foilen.smalltools.tools.FileTools;
 import com.foilen.usagemetrics.agent.AgentApp;
 import com.foilen.usagemetrics.agent.services.DockerUtils;
 import com.foilen.usagemetrics.agent.services.DockerUtilsImpl;
@@ -25,7 +22,6 @@ import com.foilen.usagemetrics.api.model.UsageResource;
 public class DockerGrabber extends AbstractBasics implements Grabber {
 
     private static final String RESOURCE_TYPE = "dockerContainer";
-    private static final String UNKNOWN_OWNER = "UNKNOWN_OWNER";
 
     private DockerUtils dockerUtils = new DockerUtilsImpl();
 
@@ -35,27 +31,11 @@ public class DockerGrabber extends AbstractBasics implements Grabber {
     public List<UsageResource> grab() {
         List<UsageResource> usageResources = new ArrayList<>();
 
-        // Get containers ownership
-        String dockerSudoDirectory = AgentApp.getAgentConfig().getDiskSpaceRootFs() + "/etc/docker-sudo/";
-        Map<String, String> ownerByContainer = new HashMap<>();
-        for (File file : new File(dockerSudoDirectory).listFiles()) {
-            if (file.getName().startsWith("containers-") && file.getName().endsWith(".conf")) {
-                int start = "containers-".length();
-                int end = file.getName().length() - ".conf".length();
-                String owner = file.getName().substring(start, end);
-                FileTools.readFileLinesStream(file).forEach(container -> ownerByContainer.put(container, owner));
-            }
-        }
-
         // Check docker containers
         dockerUtils.containerPsFindAll().forEach(container -> {
-            String owner = ownerByContainer.get(container.getName());
-            if (owner == null) {
-                owner = UNKNOWN_OWNER;
-            }
             UsageResource usageResource = new UsageResource() //
                     .setUsageResourceType(RESOURCE_TYPE) //
-                    .setOwner(owner) //
+                    .setOwner(container.getName()) //
                     .setDetails(container.getName()) //
                     .setSize(container.getTotalSize());
 
