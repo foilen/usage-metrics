2530f83be0415252ef3d6291b4ba3ad8d9890d03
Simon Levesque <simon@foilen.com>
2021-02-14 14:23:16 -0500

    Switch to Maven Central

--- create-public-release.sh
+++ create-public-release.sh
@@ -2,24 +2,27 @@
 
 set -e
 
-# Check params
-if [ $# -ne 1 ]
-	then
-		echo Usage: $0 version;
-    echo E.g: $0 0.1.0
-		echo Version is MAJOR.MINOR.BUGFIX
-		echo Latest version:
-		git describe --abbrev=0
-		exit 1;
-fi
-
-export VERSION=$1
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
+# Check params
+if [ $# -ne 1 ]
+    then
+        echo Usage: $0 version;
+        echo E.g: $0 0.1.0
+        echo Version is MAJOR.MINOR.BUGFIX
+        echo Latest version:
+        git describe --abbrev=0
+        exit 1;
+fi
+
+# Set environment
+export LANG="C.UTF-8"
+export VERSION=$1
+
 ./step-update-copyrights.sh
 ./step-clean-compile.sh
-./step-upload-bintray.sh
+./step-upload-ossrh.sh
 ./step-create-docker-image.sh
 ./step-upload-docker-image.sh
 ./step-git-tag.sh
@@ -27,4 +30,11 @@ cd $RUN_PATH
 echo ----[ Operation completed successfully ]----
 
 echo
+echo To deploy the new release:
+echo '- go on https://oss.sonatype.org/#stagingRepositories'
+echo '- close it (wait for it to be closed)'
+echo '- release it'
+echo 
+echo Then, can see published items on 
+echo https://repo1.maven.org/maven2/com/foilen/usage-metrics-api/
 echo You can send the tag: git push --tags
